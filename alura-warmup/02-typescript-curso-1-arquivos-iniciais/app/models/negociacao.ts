358d1088755e9035e2fbbe83799d2977a13a8ce9
vitor-kb <vitorbassani1@gmail.com>
2024-09-09 15:37:49 -0300

    aula 2 typescript

--- alura-warmup/02-typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ alura-warmup/02-typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,31 +1,31 @@
 export class Negociacao {//classe
-  #data; //privado
-  #quantidade; //privado
-  #valor; //privado
+  private _data: Date; //privado, se não tiver nada é public automaticamente
+  private _quantidade: number; //privado
+  private _valor: number; //privado
 
-  constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
+  constructor(data: Date, quantidade: number, valor: number) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
   //getters e setters
-  get data() {
-    return this.#data;
+  get data(): Date {
+    return this._data;
   }
 
-  get quantidade() {
-    return this.#quantidade;
+  get quantidade(): number {
+    return this._quantidade;
   }
 
-  get valor() {
-    return this.#valor;
+  get valor(): number {
+    return this._valor;
   }
 
   // set data(data) {
   //   return this.#data = data;
   // }
 
-  get volume() {
-    return this.#quantidade * this.#valor;
+  get volume(): number {
+    return this._quantidade * this._valor;
   }
 }
--- alura-warmup/02-typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
+++ alura-warmup/02-typescript-curso-1-arquivos-iniciais/dist/js/models/negociacao.js
@@ -1,31 +1,23 @@
-export class Negociacao {//classe
-  #data; //privado
-  #quantidade; //privado
-  #valor; //privado
-
-  constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
-  }
-  //getters e setters
-  get data() {
-    return this.#data;
-  }
-
-  get quantidade() {
-    return this.#quantidade;
-  }
-
-  get valor() {
-    return this.#valor;
-  }
-
-  // set data(data) {
-  //   return this.#data = data;
-  // }
-
-  get volume() {
-    return this.#quantidade * this.#valor;
-  }
+export class Negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    //getters e setters
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    // set data(data) {
+    //   return this.#data = data;
+    // }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
 }
