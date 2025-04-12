e9e3472767b9891241847a5e99448b163208273d
vitor-kb <vitorbassani1@gmail.com>
2025-04-12 13:52:28 -0300

    add modulo-microfrontend

---
+++ modulo-microfrontend/aula2-singlespa-rootconfig/account/webpack.config.js
@@ -0,0 +1,16 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "bytebank",
+    projectName: "account",
+    webpackConfigEnv,
+    argv,
+    outputSystemJS: false,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
